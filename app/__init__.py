757266e087a2964bba8140f52fe04a98ffa885c6
Admin <simonhunter28@gmail.com>
2016-10-02 17:56:10 +0100

    uncommented heroku init code

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,9 @@ db = SQLAlchemy(app)
 
 from app import views, models
 
-# if os.environ.get('HEROKU') is not None:
-#     import logging
-#     stream_handler = logging.StreamHandler()
-#     app.logger.addHandler(stream_handler)
-#     app.logger.setLevel(logging.INFO)
-#     app.logger.info('website startup')
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('website startup')
