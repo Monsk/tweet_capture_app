35cd14a64a18d89ad0187b15e77ab21473a39a0d
Admin <simonhunter28@gmail.com>
2016-10-22 14:17:39 +0100

    timezone chart updated, language freq chart added

--- app/views.py
+++ app/views.py
@@ -1,20 +1,48 @@
 from flask import Flask, render_template
-from collections import Counter
+import pandas as  pd
 
 from app import app, db
 from .models import Tweet
 
+def getTZoneFraction():
+        # Extract the timezones, count them, and then turn the counter dict into a list of tuples
+
+        tweets = pd.read_sql('tweet', db.engine)
+
+        tzones = tweets['time_zone'].dropna()
+        tzoneCounter = pd.DataFrame(tzones.value_counts())
+        tzoneCounts = [tuple(x) for x in tzoneCounter.itertuples()]
+
+        tzoneFraction = 100 * tzoneCounter / tzoneCounter.sum()
+        tzoneFraction = tzoneFraction.round(1)
+        tzoneFraction = [tuple(x) for x in tzoneFraction.itertuples()]
+
+        return tzoneFraction
+
+def getLangFraction():
+        # Extract the languages, count them, and then turn the counter dict into a list of tuples
+
+        tweets = pd.read_sql('tweet', db.engine)
+
+        languages = {'ar': 'Arabic', 'bg': 'Bulgarian', 'ca': 'Catalan', 'cs': 'Czech', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'es': 'Spanish', 'et': 'Estonian',
+                 'fa': 'Persian', 'fi': 'Finnish', 'fr': 'French', 'hi': 'Hindi', 'hr': 'Croatian', 'hu': 'Hungarian', 'id': 'Indonesian', 'is': 'Icelandic', 'it': 'Italian', 'iw': 'Hebrew',
+                 'ja': 'Japanese', 'ko': 'Korean', 'lt': 'Lithuanian', 'lv': 'Latvian', 'ms': 'Malay', 'nl': 'Dutch', 'no': 'Norwegian', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian',
+                 'ru': 'Russian', 'sk': 'Slovak', 'sl': 'Slovenian', 'sr': 'Serbian', 'sv': 'Swedish', 'th': 'Thai', 'tl': 'Filipino', 'tr': 'Turkish', 'uk': 'Ukrainian', 'ur': 'Urdu',
+                 'vi': 'Vietnamese', 'zh_CN': 'Chinese (simplified)', 'zh_TW': 'Chinese (traditional)'}
+
+        langs = tweets['lang'].dropna()
+        langCounter = pd.DataFrame(langs.value_counts())
+        langFraction = 100 * langCounter / langCounter.sum()
+        langFraction = langFraction.round(2)
+        langFraction.index = [languages[x] for x in langFraction.index]
+        langFraction = [tuple(x) for x in langFraction.itertuples()]
+
+        return langFraction
+
+
 @app.route("/")
 def main():
     return render_template("lang.html")
-
-@app.route("/top_tweets")
-def top_tweets():
-    return render_template("top_tweets.html")
-
-@app.route("/trends")
-def trends():
-    return render_template("trends.html")
 
 @app.route("/chart")
 def chart():
@@ -22,8 +50,8 @@ def chart():
 	# query or a file on the filesystem created by another script.
 	# This example expects two values per row; for more complicated examples,
 	# refer to the Google Charts gallery.
-    query = db.session.query(Tweet).all()
-    # Extract the timezones, count them, and then turn the counter dict into a list of tuples
-    timeZoneData = Counter([str(i.time_zone) for i in query]).items()
-    timeZoneData = sorted(timeZoneData, key=lambda x: x[1], reverse=True)
-    return render_template('chart.html', data=timeZoneData)
+
+    tzoneFraction = getTZoneFraction()
+    langFraction = getLangFraction()
+
+    return render_template('chart.html', **locals())
