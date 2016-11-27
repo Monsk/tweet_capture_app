30c768884ca124e6ed07010951790964cee3d4a5
Admin <simonhunter28@gmail.com>
2016-11-27 17:13:07 +0000

    Added time series of language frequency

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ import pandas as  pd
 import json, re
 from flask import (Flask, flash, Markup, redirect, render_template, request,
                    Response, session, url_for)
+from datetime import datetime, timedelta
 
 from app import app, db
 from .models import Tweet
@@ -9,47 +10,70 @@ from .models import Tweet
 # tweets = pd.read_csv('tweets.csv')
 tweets = pd.read_sql('tweet', db.engine)
 
+languages = {'ar': 'Arabic', 'bg': 'Bulgarian', 'ca': 'Catalan', 'cs': 'Czech', 'cy': 'Welsh', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'es': 'Spanish', 'et': 'Estonian',
+         'fa': 'Persian', 'fi': 'Finnish', 'fr': 'French', 'hi': 'Hindi', 'hr': 'Croatian', 'ht': 'Haitian', 'hu': 'Hungarian', 'hy': 'Armenian', 'id': 'Indonesian', 'in': 'Indonesian', 'is': 'Icelandic', 'it': 'Italian', 'iw': 'Hebrew',
+         'ja': 'Japanese', 'ko': 'Korean', 'lt': 'Lithuanian', 'lv': 'Latvian', 'ms': 'Malay', 'nl': 'Dutch', 'no': 'Norwegian', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian',
+         'ru': 'Russian', 'sk': 'Slovak', 'sl': 'Slovenian', 'sr': 'Serbian', 'sv': 'Swedish', 'th': 'Thai', 'tl': 'Filipino', 'tr': 'Turkish', 'uk': 'Ukrainian', 'und': 'Undetermined', 'ur': 'Urdu',
+         'vi': 'Vietnamese', 'zh': 'Chinese'}
 
 def getTZoneFraction(tweets):
-        # Extract the timezones, count them, and then turn the counter dict into a list of tuples
+    # Extract the timezones, count them, and then turn the counter dict into a list of tuples
 
-        tzones = tweets['time_zone'].dropna()
-        tzoneCounter = pd.DataFrame(tzones.value_counts())
-        tzoneCounts = [tuple(x) for x in tzoneCounter.itertuples()]
+    tzones = tweets['time_zone'].dropna()
+    tzoneCounter = pd.DataFrame(tzones.value_counts())
+    tzoneCounts = [tuple(x) for x in tzoneCounter.itertuples()]
 
-        tzoneFraction = 100 * tzoneCounter / tzoneCounter.sum()
-        tzoneFraction = tzoneFraction.round(1)
-        tzoneFraction = [tuple(x) for x in tzoneFraction.itertuples()]
+    tzoneFraction = 100 * tzoneCounter / tzoneCounter.sum()
+    tzoneFraction = tzoneFraction.round(1)
+    tzoneFraction = [tuple(x) for x in tzoneFraction.itertuples()]
 
-        return tzoneFraction
+    return tzoneFraction
 
 def getLangFraction(tweets):
-        # Extract the languages, count them, and then turn the counter dict into a list of tuples
+    # Extract the languages, count them, and then turn the counter dict into a list of tuples
 
-        languages = {'ar': 'Arabic', 'bg': 'Bulgarian', 'ca': 'Catalan', 'cs': 'Czech', 'cy': 'Welsh', 'da': 'Danish', 'de': 'German', 'el': 'Greek', 'en': 'English', 'es': 'Spanish', 'et': 'Estonian',
-                 'fa': 'Persian', 'fi': 'Finnish', 'fr': 'French', 'hi': 'Hindi', 'hr': 'Croatian', 'ht': 'Haitian', 'hu': 'Hungarian', 'hy': 'Armenian', 'id': 'Indonesian', 'in': 'Indonesian', 'is': 'Icelandic', 'it': 'Italian', 'iw': 'Hebrew',
-                 'ja': 'Japanese', 'ko': 'Korean', 'lt': 'Lithuanian', 'lv': 'Latvian', 'ms': 'Malay', 'nl': 'Dutch', 'no': 'Norwegian', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian',
-                 'ru': 'Russian', 'sk': 'Slovak', 'sl': 'Slovenian', 'sr': 'Serbian', 'sv': 'Swedish', 'th': 'Thai', 'tl': 'Filipino', 'tr': 'Turkish', 'uk': 'Ukrainian', 'und': 'Undetermined', 'ur': 'Urdu',
-                 'vi': 'Vietnamese', 'zh': 'Chinese'}
+    langs = tweets['lang'].dropna()
+    langCounter = pd.DataFrame(langs.value_counts())
+    langFraction = 100 * langCounter / langCounter.sum()
+    langFraction = langFraction.round(3)
+    langFraction['language'] = [languages[x] for x in langFraction.index]
+    langFraction = langFraction.rename(columns={'lang': 'percentage'})
+    langFraction = langFraction[~langFraction.index.isin(['en','und'])].head(10)
+    print(langFraction)
 
-        langs = tweets['lang'].dropna()
-        langCounter = pd.DataFrame(langs.value_counts())
-        langFraction = 100 * langCounter / langCounter.sum()
-        langFraction = langFraction.round(3)
-        langFraction['language'] = [languages[x] for x in langFraction.index]
-        langFraction = langFraction.rename(columns={'lang': 'percentage'})
-        # langFraction = [tuple(x) for x in langFraction.itertuples()]
-        # langFraction = langFraction.drop(['en','und']).head(10)
-        langFraction = langFraction[~langFraction.index.isin(['en','und'])].head(10)
-        print(langFraction)
+    return langFraction
 
-        return langFraction
+def getTimeLangFraction(tweets, commonLanguages):
+    kw = lambda x: timedelta(days=x.weekday())
+    def percentage(x):
+        percentage = 100 * float(x['count']) / float(ggTweets[ggTweets.index == x["occurred_at_week"]][0])
+        return round(percentage, 3)
+
+    tweets.occurred_at = pd.DatetimeIndex(tweets.occurred_at).normalize()
+    tweets['occurred_at_week'] = tweets.occurred_at - tweets.occurred_at.map(kw)
+    groupTweets = tweets.groupby([tweets['occurred_at_week'], 'lang']).lang.count()
+    groupTweets.rename('count', inplace=True)
+    groupTweets = groupTweets.reset_index()
+
+    ggTweets = groupTweets.groupby('occurred_at_week')['count'].sum()
+
+    groupTweets['percentage'] = groupTweets.apply(percentage, axis=1)
+    groupTweets.occurred_at_week = groupTweets.occurred_at_week.dt.strftime('%d %b %Y')
+    groupTweets = groupTweets[groupTweets.lang.isin(commonLanguages)]
+
+    return groupTweets
+
 
 
 @app.route("/")
 def main():
-    langFraction = getLangFraction(tweets).to_json(orient='records')
-    return render_template("index.html", data = langFraction)
+    langFraction = getLangFraction(tweets)
+    commonLanguages = langFraction.index
+    langFraction = langFraction.to_json(orient='records')
+    timeLangFraction = getTimeLangFraction(tweets, commonLanguages).to_json(orient='records')
+    # print(timeLangFraction)
+
+    return render_template("index.html", langData = langFraction, timeLangData = timeLangFraction)
 
 @app.route("/chart")
 def chart():
