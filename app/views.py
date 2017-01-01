d986f5b3ad93de74308126ecd29ea3c6bf6972b2
Admin <simonhunter28@gmail.com>
2017-01-01 17:29:04 +0000

    changed loading table and holding in memory to function-specific SQL queries

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from app import app, db
 from .models import Tweet
 
 # tweets = pd.read_csv('tweets.csv')
-tweets = pd.read_sql('tweet', db.engine)
+# tweets = pd.read_sql('tweet', db.engine)
 
 with open('app/languages.json') as data_file:
     languages = json.load(data_file)
@@ -16,7 +16,7 @@ with open('app/languages.json') as data_file:
 def getTZoneFraction(tweets):
     # Extract the timezones, count them, and then turn the counter dict into a list of tuples
 
-    tzones = tweets['time_zone'].dropna()
+    tzones = pd.read_sql_query("SELECT time_zone from Tweet", db.engine).iloc[:,0].dropna()
     tzoneCounter = pd.DataFrame(tzones.value_counts())
     tzoneCounts = [tuple(x) for x in tzoneCounter.itertuples()]
 
@@ -26,9 +26,9 @@ def getTZoneFraction(tweets):
 
     return tzoneFraction
 
-def getLangFraction(tweets):
+def getLangFraction():
     # Extract the languages, count them, and then turn the counter dict into a list of tuples
-    langs = tweets['lang'].dropna()
+    langs = pd.read_sql_query("SELECT lang from Tweet", db.engine).iloc[:,0].dropna()
     langCounter = pd.DataFrame(langs.value_counts())
     langFraction = 100 * langCounter / langCounter.sum()
     langFraction = langFraction.round(3)
@@ -40,12 +40,13 @@ def getLangFraction(tweets):
 
     return langFraction
 
-def getTimeLangFraction(tweets, commonLanguages):
+def getTimeLangFraction(commonLanguages):
     kw = lambda x: timedelta(days=x.weekday())
     def percentage(x):
         percentage = 100 * float(x['count']) / float(ggTweets[ggTweets.index == x["occurred_at_week"]][0])
         return round(percentage, 3)
 
+    tweets = pd.read_sql_query("SELECT lang, occurred_at from Tweet", db.engine)
     tweets.occurred_at = pd.DatetimeIndex(tweets.occurred_at).normalize()
     tweets['occurred_at_week'] = tweets.occurred_at - tweets.occurred_at.map(kw)
     groupTweets = tweets.groupby([tweets['occurred_at_week'], 'lang']).lang.count()
@@ -60,9 +61,10 @@ def getTimeLangFraction(tweets, commonLanguages):
     groupTweets = groupTweets[groupTweets.lang.isin(commonLanguages)]
     return groupTweets
 
-def getCommonSources(tweets):
+def getCommonSources():
     # Extract the tweet sources, count them, and then turn the counter dict into a list of tuples
-    sources = tweets['source_user_screen_name'].dropna()
+
+    sources = pd.read_sql_query("SELECT source_user_screen_name from Tweet", db.engine).iloc[:,0].dropna()
     sourceCounter = pd.DataFrame(sources.value_counts())
     sourceFraction = 100 * sourceCounter / sourceCounter.sum()
     sourceFraction = sourceFraction.round(3)
@@ -76,11 +78,11 @@ def getWordFrequency():
     kw = lambda x: timedelta(days=x.weekday())
     stringFilter = request.args.get('string', '', type=str)
 
-    datedTweets = tweets
-    datedTweets.occurred_at = pd.DatetimeIndex(datedTweets.occurred_at).normalize()
-    datedTweets['occurred_at_week'] = datedTweets.occurred_at - datedTweets.occurred_at.map(kw)
-    sumTweets = datedTweets.groupby(datedTweets['occurred_at_week']).text.count()
-    filterTweets = datedTweets[datedTweets.text.str.contains(stringFilter)]
+    tweets = pd.read_sql_query("SELECT text, occurred_at from Tweet", db.engine)
+    filterTweets = tweets[tweets.text.str.contains(stringFilter)]
+    filterTweets.occurred_at = pd.DatetimeIndex(filterTweets.occurred_at).normalize()
+    filterTweets['occurred_at_week'] = filterTweets.occurred_at - filterTweets.occurred_at.map(kw)
+    sumTweets = filterTweets.groupby(filterTweets['occurred_at_week']).text.count()
     wordCounts = filterTweets.groupby(filterTweets['occurred_at_week']).text.count()
 
     wordCounts = wordCounts.reset_index()
@@ -91,7 +93,6 @@ def getWordFrequency():
     wordSummary['percentage'] = (100 * wordSummary['count'] / wordSummary['sum']).round(2)
     wordSummary.occurred_at_week = wordSummary.occurred_at_week.dt.strftime('%d %b %Y')
     wordSummary['string'] = stringFilter
-    print(wordSummary.to_json(orient='records'))
     return wordSummary.to_json(orient='records')
 
 
@@ -99,11 +100,11 @@ def getWordFrequency():
 def main():
     inputString = request.args.get('a', 0, type=int)
 
-    langFraction = getLangFraction(tweets)
+    langFraction = getLangFraction()
     commonLanguages = langFraction.index
-    sourceFraction = getCommonSources(tweets).to_json(orient='records')
+    sourceFraction = getCommonSources().to_json(orient='records')
     # wordFrequency = getWordFrequency(tweets, 'brexit').to_json(orient='records')
-    timeLangFraction = getTimeLangFraction(tweets, commonLanguages).to_json(orient='records')
+    timeLangFraction = getTimeLangFraction(commonLanguages).to_json(orient='records')
 
     langFraction = langFraction.to_json(orient='records')
     topSources = json.loads(sourceFraction)
@@ -116,20 +117,3 @@ def main():
     topSources = topSources
     )
     # stringMatchData = wordFrequency)
-
-
-@app.route("/chart")
-def chart():
-	# The data can come from anywhere you can read it; for instance, an SQL
-	# query or a file on the filesystem created by another script.
-	# This example expects two values per row; for more complicated examples,
-	# refer to the Google Charts gallery.
-
-    tzoneFraction = getTZoneFraction(tweets)
-    langFraction = getLangFraction(tweets)
-
-    #Remove English from the list as it overshadows the smaller fractions, then truncate to 10 items
-    # langFraction = [i for i in langFraction if i[0] not in ('English', 'Undetermined')]
-    # del langFraction[10:]
-
-    return render_template('chart.html', **locals())
