2d39aaaad3427b9a97a2c7f41b5b857b6d6017e5
Admin <simonhunter28@gmail.com>
2017-05-21 19:42:11 +0100

    no real change, just stashing

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,31 @@ import logging
 # Create a Flask WSGI app and configure it using values from the module, and secret keys from instance/config.
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-cache = pylibmc.Client(["127.0.0.1"], binary=True)
+print(app.config)
+# cache = pylibmc.Client(["127.0.0.1"], binary=True)
+cache = pylibmc.Client(os.environ.get('MEMCACHIER_SERVERS', '').split(','),
+                    binary=True,
+                    username=os.environ.get('MEMCACHIER_USERNAME', ''),
+                    password=os.environ.get('MEMCACHIER_PASSWORD', ''),
+                    behaviors={
+                      # Faster IO
+                      "tcp_nodelay": True,
+
+                      # Keep connection alive
+                      'tcp_keepalive': True,
+
+                      # Timeout for set/get requests
+                      'connect_timeout': 2000, # ms
+                      'send_timeout': 750 * 1000, # us
+                      'receive_timeout': 750 * 1000, # us
+                      '_poll_timeout': 2000, # ms
+
+                      # Better failover
+                      'ketama': True,
+                      'remove_failed': 1,
+                      'retry_timeout': 2,
+                      'dead_timeout': 30,
+                    })
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
@@ -25,27 +49,3 @@ if os.environ.get('HEROKU') is not None:
     app.logger.addHandler(stream_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('website startup')
-
-    cache = pylibmc.Client(MEMCACHIER_SERVERS, binary=True,
-                        username=MEMCACHIER_USERNAME, password=MEMCACHIER_PASSWORD,
-                        behaviors={
-                          # Faster IO
-                          "tcp_nodelay": True,
-
-                          # Keep connection alive
-                          'tcp_keepalive': True,
-
-                          # Timeout for set/get requests
-                          'connect_timeout': 2000, # ms
-                          'send_timeout': 750 * 1000, # us
-                          'receive_timeout': 750 * 1000, # us
-                          '_poll_timeout': 2000, # ms
-
-                          # Better failover
-                          'ketama': True,
-                          'remove_failed': 1,
-                          'retry_timeout': 2,
-                          'dead_timeout': 30,
-                        })
-else:
-    print('running locally')
