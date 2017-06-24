019b68b7808dd0476aa803a72d71f05dd097c993
Admin <simonhunter28@gmail.com>
2017-06-24 15:46:59 -0700

    caching removed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template
 # from werkzeug.contrib.cache import MemcachedCache
-import pylibmc
 from flask_sqlalchemy import SQLAlchemy
 import sys
 import logging
@@ -11,7 +10,6 @@ import logging
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 
-cache = pylibmc.Client(["127.0.0.1:11211"], binary=True)
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
@@ -26,26 +24,3 @@ if os.environ.get('HEROKU') is not None:
     app.logger.addHandler(stream_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('website startup')
-    cache = pylibmc.Client(os.environ.get('MEMCACHIER_SERVERS', '').split(','),
-                        binary=True,
-                        username=os.environ.get('MEMCACHIER_USERNAME', ''),
-                        password=os.environ.get('MEMCACHIER_PASSWORD', ''),
-                        behaviors={
-                          # Faster IO
-                          "tcp_nodelay": True,
-
-                          # Keep connection alive
-                          'tcp_keepalive': True,
-
-                          # Timeout for set/get requests
-                          'connect_timeout': 2000, # ms
-                          'send_timeout': 750 * 1000, # us
-                          'receive_timeout': 750 * 1000, # us
-                          '_poll_timeout': 2000, # ms
-
-                          # Better failover
-                          'ketama': True,
-                          'remove_failed': 1,
-                          'retry_timeout': 2,
-                          'dead_timeout': 30,
-                        })
